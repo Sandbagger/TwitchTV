276ea9103dc58b825012511b56d55e80783e1f5c
Sandbagger <wneal_19@yahoo.co.uk>
2017-10-06 22:07:50 +0100

    guarantee that at least 1 user is live: WIP

--- twitchTV.js
+++ twitchTV.js
@@ -1,38 +1,57 @@
 var module = (function(){
 	var storedValue = [];
+	var storedStatus = [];
+	var featured = [];
+	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
 console.log("got to module");
 	return {
 		store: function(val){
+	
 			storedValue.push(val);
 			console.log("got to module.store");
-			dispalyjsonpPayload();
+			module.updateDisplay();
 		},
 
 		retrieve: function(){
 			return storedValue;
 		},
-		searchTerm: function(){
-			return storedValue[0][0];
+		storeStatus: function(id){
+			storedStatus.push(id);
+			console.log("got to module.store");
+			
 		},
 
-		searchResultTitles: function(){
-			return storedValue[0][1];
+		retrieveStatus: function(){
+			return storedStatus;
 		},
 
-		searchResultdescription: function(){
-			return storedValue[0][2];
+		storeFeatured: function(data){
+			featured.push(data);
+			console.log("got to module.store");
+			module.addLiveUser()
 		},
 
-		searchResultUrl: function(){
-			return storedValue[0][3];
+		retrieveFeatured: function(){
+			return featured;
 		},
+
+		addLiveUser: function(){
+			liveUser = module.retrieveFeatured()[0]["featured"][0]["stream"]["channel"]["display_name"];
+			users.push(liveUser);
+			users.forEach(requestJSONP);
+		},
+
+		returnUsers: function(){
+			return users;
+			
+		},
+
 
 		updateDisplay: function(){
 			/*splayArray();*/
-			displaySearchTerm();
-			displayTitles();
-			displaySummary();
-			updateResultsUrl()
+			displayUserInfo()
+
 		}
 	}
 
@@ -41,23 +60,40 @@ console.log("got to module");
 
 //Wait for DOM to load before adding event listener on button
 
-document.addEventListener('DOMContentLoaded', requestJSONP);
-
-var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-users.forEach(requestJSONP);
-
-
+document.addEventListener('DOMContentLoaded', getLiveStream);
 
 
 function requestJSONP(user) {
 	//dynamically create a script tag
 	var scriptTag = document.createElement("script");
 	//set url with user defined search parameter
-	scriptTag.src = "https://wind-bow.gomix.me/twitch-api/users/" + user + "?callback=module.store";
+	scriptTag.src = "https://wind-bow.gomix.me/twitch-api/users/눈꽃눈꽃" + user + "?callback=module.store";
 	//append tag to head element 
 	document.getElementsByTagName("head")[0].appendChild(scriptTag);
 	console.log("got to JSONP");
 };
+
+function requestStreamStatus(id) {
+	//dynamically create a script tag
+	var scriptTag = document.createElement("script");
+	//set url with user defined search parameter
+	scriptTag.src = "https://wind-bow.gomix.me/twitch-api/streams/" + id + "?callback=module.storeStatus";
+	//append tag to head element 
+	document.getElementsByTagName("head")[0].appendChild(scriptTag);
+	console.log("got to streamStatus");
+};
+
+function getLiveStream(){
+	//dynamically create a script tag
+	var scriptTag = document.createElement("script");
+	//set url with user defined search parameter
+	scriptTag.src = "https://wind-bow.glitch.me/twitch-api/streams/featured?callback=module.storeFeatured";
+	//append tag to head element 
+	document.getElementsByTagName("head")[0].appendChild(scriptTag);
+	console.log("got to getLiveStream");
+}
+
+
 
 function dispalyjsonpPayload(){
 var header = document.getElementsByTagName("header");
@@ -65,14 +101,16 @@ header.textContent = module.retrieve();
 }
 
 function displayUserInfo(){
-var username =  Array.prototype.slice.call(document.querySelectorAll(".user p"));
-var userImage = Array.prototype.slice.call(document.querySelectorAll(".user IMG"));
-var userInfo = module.retrieve()
+	var username =  Array.prototype.slice.call(document.querySelectorAll(".user p"));
+	var userImage = Array.prototype.slice.call(document.querySelectorAll(".user IMG"));
+	var userInfo = module.retrieve();
 
-for (var i = 0; 0 < userInfo.length; i++){
-	username[i].textContent = userInfo[i].display_name;
-	userImage[i].src = userInfo[i].logo;
-}
+	for (var i = 0; 0 < userInfo.length; i++){
+		username[i].textContent = userInfo[i].display_name;
+		userImage[i].src = userInfo[i].logo;
+		console.log(userInfo[i]["_id"]);
+		requestStreamStatus(userInfo[i]["_id"]);
+	}
 
 }
 
